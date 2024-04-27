d1c90f3b8a4f6f03345eb554aceeaeee94687ddd
zzdavix <124883392+zzdavix@users.noreply.github.com>
2024-04-27 11:53:04 -0300

    Update arvore.h

--- arvore.h
+++ arvore.h
@@ -14,5 +14,8 @@ void arv_libera (Arv* a);
 void arv_imprime (Arv* a);
 int arv_pertence (Arv* a, char c);
 ArvNo* arv_busca (Arv* a, char c);
+static int max2 (int a, int b);
+static int altura (ArvNo* r);
+int arv_altura (Arv* a);
 
 #endif
